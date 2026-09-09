960b1945b31f6cf7e7d62e6b54c0b246e9923f1a
mahesh-katta <kattamaheshreddy3@gmail.com>
2026-09-09 13:29:13 +0530

    updated exception class

--- src/main/java/com/maheshreddy/userservice/exception/ApiException.java
+++ src/main/java/com/maheshreddy/userservice/exception/ApiException.java
@@ -1,7 +1,9 @@
 package com.maheshreddy.userservice.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class ApiException extends RuntimeException {
 
     private final HttpStatus status;
@@ -11,7 +13,4 @@ public class ApiException extends RuntimeException {
         this.status = status;
     }
 
-    public HttpStatus getStatus() {
-        return status;
-    }
 }
